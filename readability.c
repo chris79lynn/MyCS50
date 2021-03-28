74e4714974aaad46f3a014c284ba43a9613084ff
Chris79Lynn <chris79lynn@gmail.com>
2021-03-28 04:37:03 +0000

    counting letters and words

--- readability.c
+++ readability.c
@@ -3,35 +3,51 @@
 #include <string.h>
 #include <ctype.h>
 
-int letter_count(void);
+int letter_count(int length, string letter_string);
+int word_count(int length, string word_string);
 
 int main (void)
 {
     // string get_letters =  get_string("Text:");
-    int alphabetic  = letter_count();
 
-    printf("%i", alphabetic);
+    string alphabetic = get_string("Text:");
+    int counter = strlen(alphabetic);
+
+    int num_letters = letter_count(counter, alphabetic);
+    printf("%i\n", num_letters);
+
+    int num_words = word_count(counter, alphabetic);
+    printf("%i", num_words);
 }
 
-int letter_count (void)
+int letter_count (int length, string letter_string)
 {
-    string input =  get_string("Text:");
-    int length = strlen(input);
+    length = strlen(letter_string);
 
-   //Test output and location of string
-   //printf("%i %i %i %i %i %i %i %i %i %i %i %i ", input[0], input[1], input[2], input[3],  input[4],  input[5],
-                                                //  input[6], input[7], input[8], input[9], input[10],input [11]);
     int letters = 0;
     for (int i = 0; i < length; i++)
     {
-      if (input [i] != 32 && isalpha(input[i]))
+      if (letter_string[i] != 32 && isalpha(letter_string[i]))
       {
           letters++;
       }
-
-
     }
     return letters;
+}
+
+int word_count(int length, string word_string)
+{
+    length = strlen(word_string);
+
+    int words = 0;
+    for (int i = 0; i < length; i++)
+    {
+      if (word_string[i] == 32)
+      {
+          words++;
+      }
+    }
+    return words+1;
 }
 
     //Before Grade 1
